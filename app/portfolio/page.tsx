662c184b69255f988c224c556438a44ad9aa3c86
Michal Špitálský <michal.spitalsky.mp@gmail.com>
2023-10-20 12:32:32 +0200

    feat: portfolio and modal

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,15 +1,67 @@
-import Image from "next/image";
+"use client";
 
-const items = [
+import Image from "next/image";
+import { Dispatch, SetStateAction, useState } from "react";
+
+type PortolioItem = {
+  name: string;
+  img: string;
+};
+
+const items: Array<PortolioItem> = [
   {
-    img: "https://images.unsplash.com/photo-1696464795756-2d92a11c504f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1937&q=80",
+    name: "Dreams on a tape",
+    img: "/img/portfolio/dreams on a tape.jpg",
   },
   {
-    img: "https://images.unsplash.com/photo-1695374885715-6bc75534f7c7?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1935&q=80",
+    name: "The Emperor",
+    img: "/img/portfolio/the emperor.jpg",
   },
 ];
 
+interface PortfolioModalProps {
+  isOpen: boolean;
+  modalToggle: Dispatch<SetStateAction<boolean>>;
+  modalImage: PortolioItem;
+}
+
+const PortfolioModal: React.FC<PortfolioModalProps> = ({
+  isOpen,
+  modalToggle,
+  modalImage,
+}) => {
+  const handleModalClose = () => {
+    modalToggle(false);
+  };
+
+  return (
+    <div
+      className="absolute top-0 left-0 w-screen h-screen overflow-hidden"
+      style={{ display: isOpen ? "block" : "none" }}
+      onClick={() => handleModalClose()}
+    >
+      <div className="absolute w-[95%] md:w-[80%] h-[90%] left-[50%] top-[50%] translate-x-[-50%] translate-y-[-50%] z-20">
+        <Image
+          src={modalImage.img}
+          alt={modalImage.name}
+          fill
+          className="object-contain"
+        />
+      </div>
+      <div className="z-10 absolute top-0 left-0 w-screen h-screen bg-black opacity-50"></div>
+    </div>
+  );
+};
+
 const PortfolioPage = () => {
+  const [modalOpen, setModal] = useState(false);
+  const [modalImage, setModalImage] = useState(items[0]);
+
+  const handleModalOpen = (newImage: PortolioItem) => {
+    setModal(true);
+    setModalImage(newImage);
+  };
+
   return (
     <>
       <h1 className="text-4xl font-black mb-4">Portfolio</h1>
@@ -17,17 +69,27 @@ const PortfolioPage = () => {
       <div className="flex flex-row flex-wrap justify-start gap-8">
         {items.map((item) => (
           <div
-            className=" aspect-square md:h-56 w-full md:w-56 overflow-hidden"
+            className="aspect-square w-full md:w-56 overflow-hidden relative portfolio-card"
             key={item.img}
+            onClick={() => handleModalOpen(item)}
           >
-            <img
-              alt={item.img}
+            <Image
+              alt={item.name}
               src={item.img}
-              className="w-full h-full object-cover"
+              fill
+              className="object-cover image"
             />
+            <div className="absolute w-full title">
+              <h3 className="font-bold">{item.name}</h3>
+            </div>
           </div>
         ))}
       </div>
+      <PortfolioModal
+        isOpen={modalOpen}
+        modalToggle={setModal}
+        modalImage={modalImage}
+      />
     </>
   );
 };

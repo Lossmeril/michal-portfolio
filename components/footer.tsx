59c1c6e20498f142fb993409e06a553b18d0d385
Michal Špitálský <michal.spitalsky.mp@gmail.com>
2023-11-06 20:11:39 +0100

    feat: dynamic year in footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,8 +1,10 @@
+const currentYear = new Date().getFullYear();
+
 const Footer = () => {
   return (
     <footer className="w-full h-12 md:h-24 flex flex-col md:flex-row flex-nowrap justify-start md:justify-between items-center lowercase py-12 text-left">
       <p className="mb-4 md:mb-0">Made with coffee, minimalism and react.js</p>
-      <p>&copy; Michal Špitálský, 2023</p>
+      <p>&copy; Michal Špitálský, {currentYear}</p>
     </footer>
   );
 };
